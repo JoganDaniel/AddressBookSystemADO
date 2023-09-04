94fe03f5720b5ef1955f6a8b18e53b9ade9da834
Jogan Daniel A <jogan.d@tvsnext.com>
2023-09-04 12:39:23 +0530

    [jogan] Add : Table created

--- AddressBookSystemADO/AddressOperations.cs
+++ AddressBookSystemADO/AddressOperations.cs
@@ -5,13 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.SqlClient;
+using System.Data;
+
 namespace AddressBookSystemADO
 {
     public class AddressOperations
     {
+        private SqlConnection con;
+        private void connection()
+        {
+            string connectionStr = "data source = (localdb)\\MSSQLLocalDB; initial catalog=AddressBookService;integrated security = true ";
+            con = new SqlConnection(connectionStr);
+        }
         public void CreateDatabase()
         {
-            SqlConnection con = new SqlConnection("data source = (localdb)\\MSSQLLocalDB; initial catalog=master;integrated security = true ");
+            connection();
             try
             {
                 string query = "Create Database AddressBookService";
@@ -23,6 +31,29 @@ namespace AddressBookSystemADO
             catch (Exception ex)
             {
                 Console.WriteLine("There is no database created ");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public void CreateTable()
+        {
+            try
+            {
+                connection();
+                SqlCommand com = new SqlCommand("AddEmployee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                string query = "Create table AddressBook(\r\nid int primary key identity(1,1),\r\nfirstName varchar(20),\r\nlastName varchar(20),\r\naddress varchar(30), \r\ncity varchar(20),\r\nstate varchar(20), \r\nzip bigint, \r\nphone varchar(10),\r\nemail varchar(30),\r\n)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                // CommandType type = CommandType.Text;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Table  Created Suucessfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Date Not Updated");
             }
             finally
             {
--- AddressBookSystemADO/Program.cs
+++ AddressBookSystemADO/Program.cs
@@ -5,6 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         AddressOperations operation = new AddressOperations();
-        operation.CreateDatabase();
+        //operation.CreateDatabase();
+        operation.CreateTable();
     }
 }

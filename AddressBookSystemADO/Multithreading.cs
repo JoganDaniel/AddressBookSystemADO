916c40018d160c149cc2834c99f23072c756f046
Jogan Daniel A <jogan.d@tvsnext.com>
2023-09-06 15:39:31 +0530

    [jogan] Add : Threads for multiple data entries

---
+++ AddressBookSystemADO/Multithreading.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystemADO
+{
+    public class Multithreading
+    {
+        AddressOperations operations = new AddressOperations();
+        public void UsingWithoutThread(List<AddressModel> list)
+        {
+            DateTime start = DateTime.Now;
+            foreach (var data in list)
+            {
+                operations.AddEmployee(data);
+            }
+            DateTime end = DateTime.Now;
+
+            Console.WriteLine("Duration without Thread " + (end - start));
+        }
+        public void UsingWithThread(List<AddressModel> list)
+        {
+            DateTime start = DateTime.Now;
+            foreach (var data in list)
+            {
+
+                Task threadss = new Task(() =>
+                {
+                    Console.WriteLine("Being Added " + data.FirstName);
+                    operations.AddEmployee(data);
+                    Console.WriteLine("Added " + data.FirstName);
+                });
+                threadss.Start();
+                threadss.Wait();
+
+            }
+            DateTime end = DateTime.Now;
+            Console.WriteLine("Duration with Thread " + (end - start));
+        }
+    }
+}
--- AddressBookSystemADO/Program.cs
+++ AddressBookSystemADO/Program.cs
@@ -45,6 +45,69 @@ public class Program
         //operation.SizeByState();
         //operation.GetPeopleInCitySortedByName("a");
         //operation.CountByType();
-        operation.PersonAsTwoRelation("kanha", "Profession");
+        //operation.PersonAsTwoRelation("kanha", "Profession");
+        List<AddressModel> list = new List<AddressModel>();
+        list.Add(new AddressModel()
+        {
+            FirstName = "a",
+            LastName = "a",
+            Address = "a",
+            City = "a",
+            State = "a",
+            Zip = 989898,
+            PhoneNumber = "9876543210",
+            Email = "a@a.com",
+            Relation = "Others"
+        });
+        list.Add(new AddressModel()
+        {
+            FirstName = "b",
+            LastName = "b",
+            Address = "b",
+            City = "b",
+            State = "b",
+            Zip = 601201,
+            PhoneNumber = "1234567890",
+            Email = "berf",
+            Relation = "Others"
+        }); list.Add(new AddressModel()
+        {
+            FirstName = "c",
+            LastName = "c",
+            Address = "c",
+            City = "c",
+            State = "c",
+            Zip = 3243432,
+            PhoneNumber = "1234567890",
+            Email = "c@wed.com",
+            Relation = "Others"
+        }); list.Add(new AddressModel()
+        {
+            FirstName = "d",
+            LastName = "d",
+            Address = "d",
+            City = "d",
+            State = "d",
+            Zip = 3489867,
+            PhoneNumber = "1234567890",
+            Email = "d@wfeqfef",
+            Relation = "Others"
+        }); list.Add(new AddressModel()
+        {
+            FirstName = "e",
+            LastName = "e",
+            Address = "e",
+            City = "e",
+            State = "e",
+            Zip = 32332,
+            PhoneNumber = "1234567890",
+            Email = "e@efef",
+            Relation = "Others"
+        });
+
+        Multithreading thread = new Multithreading();
+        thread.UsingWithThread(list);
+        thread.UsingWithThread(list);
+
     }
 }
